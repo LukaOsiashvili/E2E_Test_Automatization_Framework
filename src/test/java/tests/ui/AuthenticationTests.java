35d576a062e97d46308985839eed02690b53ca02
LukaOsiashvili <lukachosiashvili@gmail.com>
2026-02-14 02:24:48 +0400

    Added New Test Cases, Made Minor Changes

--- src/test/java/tests/ui/AuthenticationTests.java
+++ src/test/java/tests/ui/AuthenticationTests.java
@@ -1,19 +1,21 @@
 package tests.ui;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import org.example.Main;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.*;
 import tests.BaseTest;
+import ui.pages.*;
 import utils.DriverFactory;
 
 @Epic("Authentication")
+
 @Feature("Test Login, Register, and associated methods")
 public class AuthenticationTests extends BaseTest {
 
-    @Test
+    @Test(description = "Test Case 1: Full Register and Delete Account Pipeline")
+    @Description("Verify that user can register successfully with correct details, will be logged in after registration, and can delete account")
     public void registerUser(){
         DriverFactory.getDriver().get("https://automationexercise.com");
 
@@ -23,15 +25,15 @@ public class AuthenticationTests extends BaseTest {
         AccountCreatedPage accountCreatedPage = new AccountCreatedPage(DriverFactory.getDriver());
         AccountDeletePage accountDeletePage = new AccountDeletePage(DriverFactory.getDriver());
 
-        Assert.assertTrue(mainPage.isHomePageLoaded());
+        Assert.assertTrue(mainPage.isHomePageLoaded(), "Homepage has to be loaded");
 
         mainPage.getHeader().clickSignupLogin();
 
-        Assert.assertTrue(loginPage.isSignupFormVisible());
+        Assert.assertTrue(loginPage.isSignupFormVisible(), "Signup/Login Page has to be loaded and form has to be visible");
 
         loginPage.registerUser("testName", "testEmailEmail@example.com");
 
-        Assert.assertTrue(signupPage.isSignupFormVisible());
+        Assert.assertTrue(signupPage.isSignupFormVisible(), "Signup form has to be visible");
 
         signupPage.selectTitle("Mr");
         signupPage.enterName("testName");
@@ -48,16 +50,100 @@ public class AuthenticationTests extends BaseTest {
 
         signupPage.createAccount();
 
-        Assert.assertTrue(accountCreatedPage.isTitleBannerVisible());
+        Assert.assertTrue(accountCreatedPage.isTitleBannerVisible(), "Account created confirmation has to be visible");
 
         accountCreatedPage.clickContinue();
 
-        Assert.assertTrue(mainPage.getHeader().isLoggedIn());
+        Assert.assertTrue(mainPage.getHeader().isLoggedIn(), "User should be logged in after registration and indication has to be visible");
 
         mainPage.getHeader().clickDeleteAccount();
 
-        Assert.assertTrue(accountDeletePage.isTitleBannerVisible());
+        Assert.assertTrue(accountDeletePage.isTitleBannerVisible(), "Account delete confirmation has to be visible");
 
         accountDeletePage.clickContinue();
+    }
+
+    @Test(description = "Test Case 2: Login User with Correct Email and Password")
+    public void loginUserWithCorrectCredentials(){
+        DriverFactory.getDriver().get("https://automationexercise.com");
+
+        MainPage mainPage = new MainPage(DriverFactory.getDriver());
+        LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+        AccountDeletePage accountDeletePage = new AccountDeletePage(DriverFactory.getDriver());
+
+        Assert.assertTrue(mainPage.isHomePageLoaded(), "Homepage has to be loaded");
+
+        mainPage.getHeader().clickSignupLogin();
+
+        Assert.assertTrue(loginPage.isLoginFormVisible(), "Signup/Login Page has to be loaded and form has to be visible");
+
+        loginPage.enterLoginInfoAndSubmit("correctEmail@example.com", "123456789");
+
+        Assert.assertTrue(mainPage.getHeader().isLoggedIn(), "User should be logged in after registration and indication has to be visible");
+
+        mainPage.getHeader().clickDeleteAccount();
+
+        Assert.assertTrue(accountDeletePage.isTitleBannerVisible(), "Account delete confirmation has to be visible");
+
+        accountDeletePage.clickContinue();
+    }
+
+    @Test(description = "Test Case 3: Login User with Incorrect Email and Password")
+    public void loginUserWithIncorrectCredentials(){
+        DriverFactory.getDriver().get("https://automationexercise.com");
+
+        MainPage mainPage = new MainPage(DriverFactory.getDriver());
+        LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+
+        Assert.assertTrue(mainPage.isHomePageLoaded(), "Homepage has to be loaded");
+
+        mainPage.getHeader().clickSignupLogin();
+
+        Assert.assertTrue(loginPage.isLoginFormVisible(), "Signup/Login Page has to be loaded and form has to be visible");
+
+        loginPage.enterLoginInfoAndSubmit("incorrectEmail@example.com", "123456789");
+
+        Assert.assertTrue(loginPage.isIncorrectCredentialsErrorVisible(), "Error regarding using incorrect credentials should be visible");
+    }
+
+    @Test(description = "Test Case 4: Login and Logout User")
+    public void loginAndLogoutUser(){
+        DriverFactory.getDriver().get("https://automationexercise.com");
+
+        MainPage mainPage = new MainPage(DriverFactory.getDriver());
+        LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+
+        Assert.assertTrue(mainPage.isHomePageLoaded(), "Homepage has to be loaded");
+
+        mainPage.getHeader().clickSignupLogin();
+
+        Assert.assertTrue(loginPage.isLoginFormVisible(), "Signup/Login Page has to be loaded and form has to be visible");
+
+        loginPage.enterLoginInfoAndSubmit("doNotDeleteEmail@example.com", "123456789");
+
+        Assert.assertTrue(mainPage.getHeader().isLoggedIn(), "User should be logged in after registration and indication has to be visible");
+
+        mainPage.getHeader().clickLogout();
+
+        Assert.assertTrue(loginPage.isLoginFormVisible(), "user has to be navigated to login page");
+
+    }
+
+    @Test(description = "Test Case 5: Try Registering User with Already Used Email")
+    public void registerUserWithUsedEmail(){
+        DriverFactory.getDriver().get("https://automationexercise.com");
+
+        MainPage mainPage = new MainPage(DriverFactory.getDriver());
+        LoginPage loginPage = new LoginPage(DriverFactory.getDriver());
+
+        Assert.assertTrue(mainPage.isHomePageLoaded(), "Homepage has to be loaded");
+
+        mainPage.getHeader().clickSignupLogin();
+
+        Assert.assertTrue(loginPage.isSignupFormVisible(), "Signup/Login Page has to be loaded and form has to be visible");
+
+        loginPage.registerUser("name", "doNotDeleteEmail@example.com");
+
+        Assert.assertTrue(loginPage.isAlreadyUsedEmailErrorVisible(), "Error regarding registering using already existing email should be visible");
     }
 }
